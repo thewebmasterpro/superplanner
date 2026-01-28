0da50b28960a478352698cbd6c0977f38ca58c00
Anouar <anouar@thewebmaster.pro>
2026-01-29 00:03:02 +0100

    fix: Add x-supabase-client-platform to CORS headers

--- supabase/functions/send-email/index.ts
+++ supabase/functions/send-email/index.ts
@@ -4,7 +4,7 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 // Explicit CORS headers
 const corsHeaders = {
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+    'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, x-supabase-client-platform',
     'Access-Control-Allow-Methods': 'POST, OPTIONS',
 }
 
