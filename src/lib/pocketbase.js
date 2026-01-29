16a9bde54943f40d01ece2cbafc251bd60d7c137
Anouar <anouar@thewebmaster.pro>
2026-01-29 21:46:37 +0100

    Fix: Smart URL selection (Local vs Prod)

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -1,5 +1,6 @@
 import PocketBase from 'pocketbase';
 
-const pb = new PocketBase(import.meta.env.VITE_POCKETBASE_URL || 'https://pb.hagendigital.com/');
+const url = import.meta.env.VITE_POCKETBASE_URL || (import.meta.env.DEV ? 'http://127.0.0.1:8090' : 'https://pb.hagendigital.com/');
+const pb = new PocketBase(url);
 
 export default pb;
