fdc856a4c5b4db57b5e319aa176bc7c6fa81621f
Anouar <anouar@thewebmaster.pro>
2026-01-29 21:32:50 +0100

    Fix: Force production URL as default

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -1,5 +1,5 @@
 import PocketBase from 'pocketbase';
 
-const pb = new PocketBase(import.meta.env.VITE_POCKETBASE_URL || 'http://127.0.0.1:8090');
+const pb = new PocketBase(import.meta.env.VITE_POCKETBASE_URL || 'https://pb.hagendigital.com/');
 
 export default pb;
